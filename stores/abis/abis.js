1e1e8714a87f7a6e897b98f3c42d4e2c09894903
Anton Nell <antonnell.crypto@gmail.com>
2021-01-31 17:41:05 +0200

    Fix capitalisation on balancerProxyABI
    Signed-off-by: Anton Nell <antonnell.crypto@gmail.com>

--- stores/abis/abis.js
+++ stores/abis/abis.js
@@ -1,7 +1,7 @@
 import { erc20ABI } from './erc20ABI';
 import { vaultV1ABI } from './vaultV1ABI';
 import { vaultV2ABI } from './vaultV2ABI';
-import { balancerProxyABI } from './balancerProxyaBI';
+import { balancerProxyABI } from './balancerProxyABI';
 
 export const ERC20ABI = erc20ABI
 export const VAULTV1ABI = vaultV1ABI
