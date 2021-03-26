f29052d6868ac775eeb9a0d0ada29cd35a7b7c65
Patrick Aljord <patcito@gmail.com>
2021-03-26 19:05:02 +0400

    add a simulation card to get how much a vault makes it's possible to modify params (apy, token pricem years)

--- pages/invest/[address]/index.js
+++ pages/invest/[address]/index.js
@@ -1,28 +1,22 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import Layout from '../../../components/layout/layout.js'
-import {
-  Typography,
-  Paper,
-  TextField,
-  InputAdornment,
-  Button
-} from '@material-ui/core'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Layout from '../../../components/layout/layout.js';
+import { Typography, Paper, TextField, InputAdornment, Button } from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
 
-import classes from './vault.module.css'
+import classes from './vault.module.css';
 
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 
-import VaultActionCard from '../../../components/vaultActionCard'
-import VaultGrowthNumbers from '../../../components/vaultGrowthNumbers'
-import VaultPerformanceGraph from '../../../components/vaultPerformanceGraph'
-import VaultTransactions from '../../../components/vaultTransactions'
-import VaultLockupNotice from '../../../components/vaultLockupNotice'
-import VaultStrategyCard from '../../../components/vaultStrategyCard'
+import VaultActionCard from '../../../components/vaultActionCard';
+import VaultGrowthNumbers from '../../../components/vaultGrowthNumbers';
+import VaultPerformanceGraph from '../../../components/vaultPerformanceGraph';
+import VaultTransactions from '../../../components/vaultTransactions';
+import VaultLockupNotice from '../../../components/vaultLockupNotice';
+import VaultStrategyCard from '../../../components/vaultStrategyCard';
 
-import stores from '../../../stores'
+import stores from '../../../stores';
 import {
   VAULTS_UPDATED,
   GET_VAULT_PERFORMANCE,
@@ -30,141 +24,143 @@ import {
   GET_VAULT_TRANSACTIONS,
   VAULT_TRANSACTIONS_RETURNED,
   ACCOUNT_CHANGED,
-  ETHERSCAN_URL
-} from '../../../stores/constants'
+  ETHERSCAN_URL,
+} from '../../../stores/constants';
 
 function Vault(props) {
   const [, updateState] = React.useState();
   const forceUpdate = React.useCallback(() => updateState({}), []);
 
-  const router = useRouter()
+  const router = useRouter();
 
-  const storeAccount = stores.accountStore.getStore('account')
-  const [ account, setAccount ] = useState(storeAccount)
+  const storeAccount = stores.accountStore.getStore('account');
+  const [account, setAccount] = useState(storeAccount);
 
-  const storeVault = stores.investStore.getVault(router.query.address)
-  const [ vault, setVault ] = useState(storeVault)
+  const storeVault = stores.investStore.getVault(router.query.address);
+  const [vault, setVault] = useState(storeVault);
 
   const backClicked = () => {
-    router.push('/invest')
-  }
+    router.push('/invest');
+  };
 
   useEffect(() => {
     function vaultsUpdated() {
-      const v = stores.investStore.getVault(router.query.address)
-      setVault(v)
-      forceUpdate()
+      const v = stores.investStore.getVault(router.query.address);
+      setVault(v);
+      forceUpdate();
     }
 
-    stores.emitter.on(VAULTS_UPDATED, vaultsUpdated)
-    stores.emitter.on(VAULT_PERFORMANCE_RETURNED, vaultsUpdated)
-    stores.emitter.on(VAULT_TRANSACTIONS_RETURNED, vaultsUpdated)
+    stores.emitter.on(VAULTS_UPDATED, vaultsUpdated);
+    stores.emitter.on(VAULT_PERFORMANCE_RETURNED, vaultsUpdated);
+    stores.emitter.on(VAULT_TRANSACTIONS_RETURNED, vaultsUpdated);
 
     return () => {
-      stores.emitter.removeListener(VAULTS_UPDATED, vaultsUpdated)
-      stores.emitter.removeListener(VAULT_PERFORMANCE_RETURNED, vaultsUpdated)
-      stores.emitter.removeListener(VAULT_TRANSACTIONS_RETURNED, vaultsUpdated)
-    }
-  }, [])
+      stores.emitter.removeListener(VAULTS_UPDATED, vaultsUpdated);
+      stores.emitter.removeListener(VAULT_PERFORMANCE_RETURNED, vaultsUpdated);
+      stores.emitter.removeListener(VAULT_TRANSACTIONS_RETURNED, vaultsUpdated);
+    };
+  }, []);
 
   useEffect(() => {
     const accountChanged = () => {
-      const storeAccount = stores.accountStore.getStore('account')
-      setAccount(storeAccount)
-    }
+      const storeAccount = stores.accountStore.getStore('account');
+      setAccount(storeAccount);
+    };
 
-    stores.emitter.on(ACCOUNT_CHANGED, accountChanged)
+    stores.emitter.on(ACCOUNT_CHANGED, accountChanged);
     return () => {
-      stores.emitter.removeListener(ACCOUNT_CHANGED, accountChanged)
-    }
-  }, [])
+      stores.emitter.removeListener(ACCOUNT_CHANGED, accountChanged);
+    };
+  }, []);
 
   useEffect(() => {
-    stores.dispatcher.dispatch({ type: GET_VAULT_PERFORMANCE, content: { address: router.query.address, duration: 'Month' } })
-    stores.dispatcher.dispatch({ type: GET_VAULT_TRANSACTIONS, content: { address: router.query.address } })
-  }, [])
+    stores.dispatcher.dispatch({ type: GET_VAULT_PERFORMANCE, content: { address: router.query.address, duration: 'Month' } });
+    stores.dispatcher.dispatch({ type: GET_VAULT_TRANSACTIONS, content: { address: router.query.address } });
+  }, []);
 
-  const vaultType = (vault.type === 'v2' && !vault.endorsed) ? 'Exp' : vault.type
+  const vaultType = vault.type === 'v2' && !vault.endorsed ? 'Exp' : vault.type;
 
-  let vaultTypeClass = null
+  let vaultTypeClass = null;
   switch (vaultType) {
     case 'v1':
-      vaultTypeClass = classes.vaultV1VersionContainer
+      vaultTypeClass = classes.vaultV1VersionContainer;
       break;
     case 'v2':
-      vaultTypeClass = classes.vaultV2VersionContainer
+      vaultTypeClass = classes.vaultV2VersionContainer;
       break;
     case 'Exp':
-      vaultTypeClass = classes.vaultExpVersionContainer
+      vaultTypeClass = classes.vaultExpVersionContainer;
       break;
     case 'Earn':
-      vaultTypeClass = classes.vaultEarnVersionContainer
+      vaultTypeClass = classes.vaultEarnVersionContainer;
       break;
     case 'Lockup':
-      vaultTypeClass = classes.vaultLockupVersionContainer
+      vaultTypeClass = classes.vaultLockupVersionContainer;
       break;
     default:
-      vaultTypeClass = classes.vaultVersionContainer
+      vaultTypeClass = classes.vaultVersionContainer;
       break;
   }
 
   const onVaultClicked = () => {
-    window.open(`${ETHERSCAN_URL}address/${vault.address}`)
-  }
+    window.open(`${ETHERSCAN_URL}address/${vault.address}`);
+  };
 
   return (
-    <Layout changeTheme={ props.changeTheme } backClicked={ backClicked }>
+    <Layout changeTheme={props.changeTheme} backClicked={backClicked}>
       <Head>
         <title>Invest</title>
       </Head>
-      <div className={ classes.vaultContainer }>
-        <Paper elevation={ 0 } className={ classes.overviewContainer }>
-          <div className={ classes.vaultStatsContainer }>
-            <div className={ classes.vaultBalanceContainer } onClick={ onVaultClicked }>
-              <div className={ classes.vaultOutline } >
-                <div className={ classes.vaultLogo }>
-                  { !vault ? <Skeleton /> : <img src={ vault.icon ? vault.icon : '/tokens/unknown-logo.png' } className={ classes.vaultIcon } alt='' width={ 50 } height={ 50 } /> }
+      <div className={classes.vaultContainer}>
+        <Paper elevation={0} className={classes.overviewContainer}>
+          <div className={classes.vaultStatsContainer}>
+            <div className={classes.vaultBalanceContainer} onClick={onVaultClicked}>
+              <div className={classes.vaultOutline}>
+                <div className={classes.vaultLogo}>
+                  {!vault ? (
+                    <Skeleton />
+                  ) : (
+                    <img src={vault.icon ? vault.icon : '/tokens/unknown-logo.png'} className={classes.vaultIcon} alt="" width={50} height={50} />
+                  )}
                 </div>
               </div>
-              <div className={ classes.vaultTitle }>
-                <Typography variant='h1'>
-                  { !vault ? <Skeleton /> : vault.displayName }
-                </Typography>
-                <div className={ vaultTypeClass }>
-                  <Typography  className={ classes.vaultVersionText }>{ vaultType } Vault</Typography>
+              <div className={classes.vaultTitle}>
+                <Typography variant="h1">{!vault ? <Skeleton /> : vault.displayName}</Typography>
+                <div className={vaultTypeClass}>
+                  <Typography className={classes.vaultVersionText}>{vaultType} Vault</Typography>
                 </div>
               </div>
             </div>
           </div>
-            {
-              vault.strategies.map((strategy) => {
-                return (<React.Fragment>
-                  <div className={ classes.separator }></div>
-                  <div className={ classes.overviewCard }>
-                    <VaultStrategyCard strategy={ strategy } vault={ vault } />
-                  </div>
-                </React.Fragment>)
-              })
-            }
+          {vault.strategies.map((strategy) => {
+            return (
+              <React.Fragment>
+                <div className={classes.separator}></div>
+                <div className={classes.overviewCard}>
+                  <VaultStrategyCard strategy={strategy} vault={vault} />
+                </div>
+              </React.Fragment>
+            );
+          })}
         </Paper>
-        <div className={ classes.vaultInfo }>
-          <div className={ classes.cardSeparation }>
-            <VaultActionCard vault={ vault } />
+        <div className={classes.vaultInfo}>
+          <div className={classes.cardSeparation}>
+            <VaultActionCard vault={vault} />
           </div>
-          <div className={ classes.borderedSection }>
-            <div className={ classes.cardSeparation }>
-              { account && account.address && <VaultGrowthNumbers vault={ vault } />}
-              { vaultType !== 'Lockup' && <VaultPerformanceGraph vault={ vault } />}
-              { vaultType === 'Lockup' && <VaultLockupNotice vault={ vault } account={ account } /> }
+          <div className={classes.borderedSection}>
+            <div className={classes.cardSeparation}>
+              {account && account.address && <VaultGrowthNumbers vault={vault} />}
+              {vaultType !== 'Lockup' && <VaultPerformanceGraph vault={vault} />}
+              {vaultType === 'Lockup' && <VaultLockupNotice vault={vault} account={account} />}
             </div>
           </div>
         </div>
-        <div className={ classes.vaultTransactionsContainer }>
-          { account && account.address && vaultType !== 'Earn' && vaultType !== 'Lockup' && <VaultTransactions vault={ vault } /> }
+        <div className={classes.vaultTransactionsContainer}>
+          {account && account.address && vaultType !== 'Earn' && vaultType !== 'Lockup' && <VaultTransactions vault={vault} />}
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Vault
+export default Vault;
