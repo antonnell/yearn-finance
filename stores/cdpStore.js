00f87afcb2aa4c1bc51a012d30cc8fa6944aedc0
antonnell <antonnell.crpto@gmail.com>
2021-03-28 20:06:03 +0200

    fix to this.etmitter instead of this.emitter.
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- stores/cdpStore.js
+++ stores/cdpStore.js
@@ -424,7 +424,7 @@ class Store {
     } catch(ex) {
       console.log(ex)
       this.emitter.emit(CDP_CONFIGURED);
-      this.etmiter.emit(ERROR, ex)
+      this.emitter.emit(ERROR, ex)
     }
   };
 
