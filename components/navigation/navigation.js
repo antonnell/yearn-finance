d29c91900e5b3535c5ad0c514aabf0fa43dceacf
antonnell <antonnell.crpto@gmail.com>
2021-06-12 11:21:01 +0200

    Version 1.2.0 Makebetter Consulting updates.
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -627,7 +627,7 @@ function Navigation(props) {
         </Grid>
         </Grid>
       </div>
-      <Typography className={classes.smallVersion}>Version 1.1.12</Typography>
+      <Typography className={classes.smallVersion}>Version 1.2.0</Typography>
     </Paper>
   );
 }
