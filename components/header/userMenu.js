13bb88a49ff8446f03d9fca9d45a6fefd5792f91
zee <user@KING-ZEE-MacBook.local>
2021-07-20 08:57:10 +0200

    logging function fixed

--- components/header/userMenu.js
+++ components/header/userMenu.js
@@ -86,7 +86,7 @@ export default function CustomizedMenus(props) {
     if (account && account.address) {
       setAnchorEl(event.currentTarget);
     } else {
-      loginClicked;
+      loginClicked()
     }
   };
 
