bcb2e84fe3ed77a9ecc055d0b4317ebaadb5eef9
antonnell <antonnell.crypto@gmail.com>
2021-07-23 12:07:46 +0200

    Version 1.3.1
    System JSON updated to latest.
    Invalid chain message updated.
    CDP section temporarily disabled.
    
    Signed-off-by: antonnell <antonnell.crypto@gmail.com>

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -629,7 +629,7 @@ function Navigation(props) {
         </Grid>
         </Grid>
       </div>
-      <Typography className={classes.smallVersion}>Version 1.3.1</Typography>
+      <Typography className={classes.smallVersion}>Version 1.3.2</Typography>
     </Paper>
   );
 }
