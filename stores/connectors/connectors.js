263b03a59987d14e6b11b45ed754b3ddc9b36276
antonnell <antonnell.crypto@gmail.com>
2021-07-23 12:04:49 +0200

    improved listener for when chainID is invalid. swapping chainID to Mainnet now also updates the UI.
    Signed-off-by: antonnell <antonnell.crypto@gmail.com>

--- stores/connectors/connectors.js
+++ stores/connectors/connectors.js
@@ -20,7 +20,7 @@ const RPC_URLS = {
 export const network = new NetworkConnector({ urls: { 1: RPC_URLS[1] } });
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42]
+  supportedChainIds: [1]
 });
 
 export const walletconnect = new WalletConnectConnector({
