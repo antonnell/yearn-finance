554eeda5e34747d2bded54a45bfda75044872398
antonnell <antonnell.crypto@gmail.com>
2021-08-10 13:44:53 +0200

    added plausible. updated system configuration info.
    Signed-off-by: antonnell <antonnell.crypto@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script defer data-domain="yearn.fi" src="https://plausible.io/js/plausible.js"></script>
           <link rel="stylesheet" href="/fonts/Inter/Inter.css" />
           <link rel="stylesheet" href="/fonts/Druk/Druk.css" />
           <script type="text/javascript" src="/static/noFlicker.js"></script>
