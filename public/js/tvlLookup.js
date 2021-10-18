649228b55c09249b01e7ffccbfff5117aa8baa2f
antonnell <antonnell.crypto@gmail.com>
2021-10-18 10:00:58 +0200

    Added TVL lookup and TVL change for landing.html
    Signed-off-by: antonnell <antonnell.crypto@gmail.com>

--- public/js/tvlLookup.js
+++ public/js/tvlLookup.js
@@ -12,7 +12,17 @@ $(document).ready(function(){
     : Math.abs(Number(labelValue));
   }
 
-  //only doing current TVL
+  function formatDateToObjectKey (date) {
+    var mm = date.getMonth() + 1; // getMonth() is zero-based
+    var dd = date.getDate();
+
+    return `${[date.getFullYear(),
+            (mm>9 ? '' : '0') + mm,
+            (dd>9 ? '' : '0') + dd
+          ].join('-')}T00:00:00+00:00`;
+  }
+
+  //current TVL
   const URL = 'https://yearn.science/v1/tvl/latest'
   $.ajax({ url: URL,
     context: document.body,
@@ -21,52 +31,43 @@ $(document).ready(function(){
       $('#tvl-current').html(`$ ${convertToInternationalCurrencySystem(tvl)}`);
     }});
 
+  // historic TVL change
+  const HISTORIC_URL = 'https://yearn.science/v1/tvl'
+  $.ajax({ url: HISTORIC_URL,
+    context: document.body,
+    success: function(response){
+      const date = new Date()
 
+      let today = formatDateToObjectKey(date)
 
+      date.setDate(date.getDate()-1);
+      let yesterday = formatDateToObjectKey(date)
 
-  //
-  // const currentTimestamp = Date.now() / 1000 | 0
-  //
-  // $.ajax({ url: `https://ec2-3-239-19-233.compute-1.amazonaws.com/api/query?query=sum%28yearn%7Bparam%3D%22tvl%22%7D%29+%2B+avg%28yearn%7Bparam%3D%22vecrv+balance%22%7D%29+*+avg%28yearn%7Bparam%3D%22crv+price%22%7D%29+%2B+sum%28yearn_vault%7Bparam%3D%22tvl%22%7D%29+%2B+sum%28iearn%7Bparam%3D%22tvl%22%7D%29+%2B+sum%28ironbank%7Bparam%3D%22tvl%22%7D%29+-+sum%28yearn%7Bparam%3D%22tvl%22%2Cvault%3D~%22curve.fi%2F%28y%7Cbusd%29%22%7D%29&time=${currentTimestamp}`,
-  //   context: document.body,
-  //   success: function(response){
-  //     let tvl = response?.data?.result[0]?.value[1]
-  //
-  //     $('#tvl-current').html(`$ ${convertToInternationalCurrencySystem(tvl)}`);
-  //   }});
-  //
-  // $.ajax({ url: `https://ec2-3-239-19-233.compute-1.amazonaws.com/api/query_range?query=sum(yearn%7Bparam%3D%22tvl%22%7D)%20%2B%20avg(yearn%7Bparam%3D%22vecrv%20balance%22%7D)%20*%20avg(yearn%7Bparam%3D%22crv%20price%22%7D)%20%2B%20sum(yearn_vault%7Bparam%3D%22tvl%22%7D)%20%2B%20sum(iearn%7Bparam%3D%22tvl%22%7D)%20%2B%20sum(ironbank%7Bparam%3D%22tvl%22%7D)%20-%20sum(yearn%7Bparam%3D%22tvl%22%2Cvault%3D~%22curve.fi%2F(y%7Cbusd)%22%7D)&&start=${currentTimestamp-86400}&end=${currentTimestamp}&step=86400`,
-  //   context: document.body,
-  //   success: function(response){
-  //     let tvlYesterday = response?.data?.result[0]?.values[0][1]
-  //     let tvlNow = response?.data?.result[0]?.values[1][1]
-  //
-  //     let isLoss = false
-  //     let tvlChange = tvlNow - tvlYesterday
-  //     if(tvlChange < 0) {
-  //       tvlChange = tvlChange * -1
-  //       isLoss = true
-  //     }
-  //
-  //     $('#w-node-_46935bc9-0220-5ce3-3a9e-71589d3bc51a-8b22666b').html(`${isLoss ? '-' : '+'} $ ${convertToInternationalCurrencySystem(tvlChange)}`);
-  //     $('#w-node-f7911679-36ee-dfee-8fef-6ecc33cb620b-8b22666b').html(`${isLoss ? '▼' : '▲'}`).addClass(isLoss ? 'indicator-down' : 'indicator-up').removeClass(isLoss ? 'indicator-up' : 'indicator-up');
-  //
-  //   }});
-  //
-  // $.ajax({ url: `https://ec2-3-239-19-233.compute-1.amazonaws.com/api/query_range?query=sum(yearn%7Bparam%3D%22tvl%22%7D)%20%2B%20avg(yearn%7Bparam%3D%22vecrv%20balance%22%7D)%20*%20avg(yearn%7Bparam%3D%22crv%20price%22%7D)%20%2B%20sum(yearn_vault%7Bparam%3D%22tvl%22%7D)%20%2B%20sum(iearn%7Bparam%3D%22tvl%22%7D)%20%2B%20sum(ironbank%7Bparam%3D%22tvl%22%7D)%20-%20sum(yearn%7Bparam%3D%22tvl%22%2Cvault%3D~%22curve.fi%2F(y%7Cbusd)%22%7D)&&start=${currentTimestamp-2592000}&end=${currentTimestamp}&step=86400`,
-  //   context: document.body,
-  //   success: function(response){
-  //     let tvlLastMonth = response?.data?.result[0]?.values[0][1]
-  //     let tvlNow = response?.data?.result[0]?.values[response?.data?.result[0]?.values.length-1][1]
-  //
-  //     let isLoss = false
-  //     let tvlChange = tvlNow - tvlLastMonth
-  //     if(tvlChange < 0) {
-  //       tvlChange = tvlChange * -1
-  //       isLoss = true
-  //     }
-  //
-  //     $('#w-node-_3ba17473-150a-885c-a2a3-ec0f4c9f55cf-8b22666b').html(`${isLoss ? '-' : '+'} $ ${convertToInternationalCurrencySystem(tvlChange)}`);
-  //     $('#w-node-_3ba17473-150a-885c-a2a3-ec0f4c9f55d1-8b22666b').html(`${isLoss ? '▼' : '▲'}`).addClass(isLoss ? 'indicator-down' : 'indicator-up').removeClass(isLoss ? 'indicator-up' : 'indicator-up');;
-  //   }});
+      date.setDate(date.getDate()-29);
+      let last_month = formatDateToObjectKey(date)
+
+      let tvl = response[today]
+      let tvl_yesterday = response[yesterday]
+      let tvl_last_month = response[last_month]
+
+      let isLoss = false
+      let tvlChange = tvl - tvl_yesterday
+      if(tvlChange < 0) {
+        tvlChange = tvlChange * -1
+        isLoss = true
+      }
+
+      $('#tvl-day').html(`${isLoss ? '-' : '+'} $ ${convertToInternationalCurrencySystem(tvlChange)}`);
+      $('#tvl-day-indicator').html(`${isLoss ? '▼' : '▲'}`).addClass(isLoss ? 'indicator-down' : 'indicator-up').removeClass(isLoss ? 'indicator-up' : 'indicator-down');
+
+      isLoss = false
+      tvlChange = tvl - tvl_last_month
+      if(tvlChange < 0) {
+        tvlChange = tvlChange * -1
+        isLoss = true
+      }
+
+      $('#tvl-month').html(`${isLoss ? '-' : '+'} $ ${convertToInternationalCurrencySystem(tvlChange)}`);
+      $('#tvl-month-indicator').html(`${isLoss ? '▼' : '▲'}`).addClass(isLoss ? 'indicator-down' : 'indicator-up').removeClass(isLoss ? 'indicator-up' : 'indicator-down');
+    }});
 });
